99cb19156d4c3245e98a48ef1c80ace60c0d8a89
ruchou <joe04024@gapp.nthu.edu.tw>
2019-03-11 18:58:00 +0800

    fix client testimony to I believe

--- src/Components/Testimonials.js
+++ src/Components/Testimonials.js
@@ -20,7 +20,7 @@ class Testimonials extends Component {
          <div className="row">
 
             <div className="two columns header-col">
-               <h1><span>Client Testimonials</span></h1>
+               <h1><span>I believe</span></h1>
             </div>
 
             <div className="ten columns flex-container">
