fbf3d55c872a795bca5469d013e2fadfdbfa6208
ruchou <joe04024@gapp.nthu.edu.tw>
2019-03-12 23:10:51 +0800

    add google analytics

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -28,7 +28,7 @@ class Header extends Component {
             <li><a className="smoothscroll" href="#about">About</a></li>
 	         <li><a className="smoothscroll" href="#resume">Resume</a></li>
             {/*<li><a className="smoothscroll" href="#portfolio">Works</a></li>*/}
-            <li><a className="smoothscroll" href="#testimonials">Testimonials</a></li>
+            <li><a className="smoothscroll" href="#testimonials">Believe</a></li>
             <li><a className="smoothscroll" href="#contact">Contact</a></li>
          </ul>
 
