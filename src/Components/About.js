3d38553643eb87173c6eb95d40ee8cee127e2c88
Ru Chou (YAN-RU JHOU) <joe04024@gapp.nthu.edu.tw>
2019-04-04 16:42:24 +0800

    add cv

--- src/Components/About.js
+++ src/Components/About.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import PDF from '../JhouYanRu-CV.pdf'
 
 class About extends Component {
   render() {
@@ -38,7 +39,7 @@ class About extends Component {
                </div>
                <div className="columns download">
                   <p>
-                     <a href={resumeDownload} target="_blank" download className="button"><i className="fa fa-download"></i>Download Resume</a>
+                     <a href={PDF} target = "_blank" className="button"><i className="fa fa-download"></i>Download Resume</a>
                   </p>
                </div>
             </div>
