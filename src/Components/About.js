0d0424fc6fc57e49b6b410e51c0b51b5d6bacd8f
Ru Chou (YAN-RU JHOU) <joe04024@gapp.nthu.edu.tw>
2019-04-04 16:20:22 +0800

    add cv

--- src/Components/About.js
+++ src/Components/About.js
@@ -13,7 +13,7 @@ class About extends Component {
       var zip = this.props.data.address.zip;
       var phone= this.props.data.phone;
       var email = this.props.data.email;
-      var resumeDownload = this.props.data.resumedownload;
+      var resumeDownload = "files/"+this.props.data.resumedownload;
     }
 
     return (
