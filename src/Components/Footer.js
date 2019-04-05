e2c2b4ee346b5b3fbfa21df931f6c068d63381c7
Ru Chou (YAN-RU JHOU) <joe04024@gapp.nthu.edu.tw>
2019-04-05 21:37:56 +0800

    modify copyright

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -19,7 +19,7 @@ class Footer extends Component {
            </ul>
 
            <ul className="copyright">
-              <li>&copy; Copyright 2017 Tim Baker</li>
+              <li>&copy; Copyright 2019 Yan-Ru Jhou</li>
               <li>Design by <a title="Styleshout" href="http://www.styleshout.com/">Styleshout</a></li>
            </ul>
 
