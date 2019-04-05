0a5f8055922556dea68953e4216bf421e93dfa01
Ru Chou (YAN-RU JHOU) <joe04024@gapp.nthu.edu.tw>
2019-04-05 22:01:15 +0800

    modify copyright

--- src/Components/Resume.js
+++ src/Components/Resume.js
@@ -71,8 +71,8 @@ class Resume extends Component {
 
          <div className="nine columns main-col">
 
-            <p>{skillmessage}
-            </p>
+            {/*<p>{skillmessage}*/}
+            {/*</p>*/}
 
 				<div className="bars">
 				   <ul className="skills">
