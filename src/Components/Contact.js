f8f9fd9ce2a02cf727b69e63700bb7c739adc16a
dh4u <40501523+dh4u@users.noreply.github.com>
2019-04-25 16:10:49 -0400

    Update Contact.js - Remove "/>" From Showing
    In the Contact section the Subject and Message labels have a "/>" above them when viewed in the browser.

--- src/Components/Contact.js
+++ src/Components/Contact.js
@@ -46,12 +46,12 @@ class Contact extends Component {
 
                   <div>
 						   <label htmlFor="contactEmail">Email <span className="required">*</span></label>
-						   <input type="text" defaultValue="" size="35" id="contactEmail" name="contactEmail" onChange={this.handleChange}/>/>
+						   <input type="text" defaultValue="" size="35" id="contactEmail" name="contactEmail" onChange={this.handleChange}/>
                   </div>
 
                   <div>
 						   <label htmlFor="contactSubject">Subject</label>
-						   <input type="text" defaultValue="" size="35" id="contactSubject" name="contactSubject" onChange={this.handleChange}/>/>
+						   <input type="text" defaultValue="" size="35" id="contactSubject" name="contactSubject" onChange={this.handleChange}/>
                   </div>
 
                   <div>
