8e90dc7c1af37286d1efc52edc91d189dd5872b8
yanrujhou <joe04024@gmail.com>
2019-11-21 23:29:18 -0800

    code refactor

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,63 +1,57 @@
 import React, { Component } from 'react';
 
 class Header extends Component {
-  render() {
-
-    if(this.props.data){
-      var name = this.props.data.name;
-      var occupation= this.props.data.occupation;
-      var description= this.props.data.description.map((ele,key)=>{
-        return <h3 key={key}>{ele}</h3>
-      });
-      var city= this.props.data.address.city;
-      var networks= this.props.data.social.map(function(network){
-        return <li key={network.name}><a href={network.url}><i className={network.className}></i></a></li>
-      })
-      var country=this.props.data.address.country;
-      var school = this.props.data.currentSchool;
+    constructor(props){
+        super(props);
     }
-
-
-
+    render() {
+        if(this.props.data){
+            var name = this.props.data.name;
+            var occupation= this.props.data.occupation;
+            var description= this.props.data.description.map((ele,key)=>{
+                return <h3 key={key}>{ele}</h3>
+            });
+            var city= this.props.data.address.city;
+            var networks= this.props.data.social.map(function(network){
+                return <li key={network.name}><a href={network.url}><i className={network.className}></i></a></li>
+            });
+            var country=this.props.data.address.country;
+            var school = this.props.data.currentSchool;
+        }
 
     return (
-      <header id="home">
+        <header id="home">
+            <nav id="nav-wrap">
+                <a className="mobile-btn" href="#nav-wrap" title="Show navigation">Show navigation</a>
+                <a className="mobile-btn" href="#home" title="Hide navigation">Hide navigation</a>
 
-      <nav id="nav-wrap">
+                <ul id="nav" className="nav">
+                    <li className="current"><a className="smoothscroll" href="#home">Home</a></li>
+                    <li><a className="smoothscroll" href="#about">About</a></li>
+	                <li><a className="smoothscroll" href="#resume">Resume</a></li>
+                    {/*<li><a className="smoothscroll" href="#portfolio">Works</a></li>*/}
+                    <li><a className="smoothscroll" href="#testimonials">Believe</a></li>
+                    <li><a className="smoothscroll" href="#contact">Contact</a></li>
+                </ul>
+            </nav>
 
-         <a className="mobile-btn" href="#nav-wrap" title="Show navigation">Show navigation</a>
-	      <a className="mobile-btn" href="#home" title="Hide navigation">Hide navigation</a>
-
-         <ul id="nav" className="nav">
-            <li className="current"><a className="smoothscroll" href="#home">Home</a></li>
-            <li><a className="smoothscroll" href="#about">About</a></li>
-	         <li><a className="smoothscroll" href="#resume">Resume</a></li>
-            {/*<li><a className="smoothscroll" href="#portfolio">Works</a></li>*/}
-            <li><a className="smoothscroll" href="#testimonials">Believe</a></li>
-            <li><a className="smoothscroll" href="#contact">Contact</a></li>
-         </ul>
-
-      </nav>
-
-      <div className="row banner">
-         <div className="banner-text">
-            <h1 className="responsive-headline">I'm {name}</h1>
-            <h2>I'm a {occupation} at {school}</h2>
-            <div>
-                {description}
+            <div className="row banner">
+                <div className="banner-text">
+                    <h1 className="responsive-headline">I'm {name}</h1>
+                    <h2>I'm a {occupation} at {school}</h2>
+                    <div>
+                        {description}
+                    </div>
+                    <hr/>
+                    <ul className="social">
+                        {networks}
+                    </ul>
+                </div>
             </div>
-            <hr />
-            <ul className="social">
-               {networks}
-            </ul>
-         </div>
-      </div>
-
-      <p className="scrolldown">
-         <a className="smoothscroll" href="#about"><i className="icon-down-circle"></i></a>
-      </p>
-
-   </header>
+            <p className="scrolldown">
+            <a className="smoothscroll" href="#about"><i className="icon-down-circle"></i></a>
+            </p>
+        </header>
     );
   }
 }
