d05ad554af57ca4f127fb34fe44d6744771b3f50
yanrujhou <joe04024@gmail.com>
2019-11-21 23:35:02 -0800

    code refactor

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import ReactGA from 'react-ga';
 import $ from 'jquery';
 import './App.css';
@@ -12,56 +12,51 @@ import Portfolio from './Components/Portfolio';
 
 
 class App extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            foo: 'bar',
+            resumeData: {}
+        };
+        this.id = "UA-131462650-2";
+        ReactGA.initialize(this.id);
+        ReactGA.pageview(window.location.pathname);
+
+    }
+
+    getResumeData() {
+        $.ajax({
+            url: '/resumeData.json',
+            dataType: 'json',
+            cache: false,
+            success: function (data) {
+                this.setState({resumeData: data});
+            }.bind(this),
+            error: function (xhr, status, err) {
+                console.log(err);
+                alert(err);
+            }
+        });
+    }
+
+    componentDidMount() {
+        this.getResumeData();
+    }
 
 
-  constructor(props){
-    super(props);
-    this.state = {
-      foo: 'bar',
-      resumeData: {}
-    };
-    this.id="UA-131462650-2";
-
-    ReactGA.initialize(this.id);
-    ReactGA.pageview(window.location.pathname);
-
-  }
-
-  getResumeData(){
-    $.ajax({
-      url:'/resumeData.json',
-      dataType:'json',
-      cache: false,
-      success: function(data){
-        this.setState({resumeData: data});
-      }.bind(this),
-      error: function(xhr, status, err){
-        console.log(err);
-        alert(err);
-      }
-    });
-  }
-
-  componentDidMount(){
-    this.getResumeData();
-  }
-
-
-
-
-  render() {
-    return (
-      <div className="App">
-        <Header data={this.state.resumeData.main}/>
-        <About data={this.state.resumeData.main}/>
-        <Resume data={this.state.resumeData.resume}/>
-        {/*<Portfolio data={this.state.resumeData.portfolio}/>*/}
-        <Testimonials data={this.state.resumeData.testimonials}/>
-        <Contact data={this.state.resumeData.main}/>
-        <Footer data={this.state.resumeData.main}/>
-      </div>
-    );
-  }
+    render() {
+        return (
+            <div className="App">
+                <Header data={this.state.resumeData.main}/>
+                <About data={this.state.resumeData.main}/>
+                <Resume data={this.state.resumeData.resume}/>
+                {/*<Portfolio data={this.state.resumeData.portfolio}/>*/}
+                <Testimonials data={this.state.resumeData.testimonials}/>
+                <Contact data={this.state.resumeData.main}/>
+                <Footer data={this.state.resumeData.main}/>
+            </div>
+        );
+    }
 }
 
 export default App;
--- src/Components/About.js
+++ src/Components/About.js
@@ -1,61 +1,60 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import PDF from '../JhouYanRuResume.pdf'
 
 class About extends Component {
-  render() {
+    render() {
 
-    if(this.props.data){
-      var name = this.props.data.name;
-      var profilepic= "images/"+this.props.data.image;
-      var bio = this.props.data.bio;
-      var street = this.props.data.address.street;
-      var city = this.props.data.address.city;
-      var state = this.props.data.address.state;
-      var zip = this.props.data.address.zip;
-      // var phone= this.props.data.phone;
-      var email = this.props.data.email;
-      var resumeDownload = "files/"+this.props.data.resumedownload;
+        if (this.props.data) {
+            var name = this.props.data.name;
+            var profilepic = "images/" + this.props.data.image;
+            var bio = this.props.data.bio;
+            var street = this.props.data.address.street;
+            var city = this.props.data.address.city;
+            var state = this.props.data.address.state;
+            var zip = this.props.data.address.zip;
+            // var phone= this.props.data.phone;
+            var email = this.props.data.email;
+            var resumeDownload = "files/" + this.props.data.resumedownload;
 
-      var phone = this.props.data.phone.map(function (eachPhone,ind) {
-                return <li key={ind}>{eachPhone}</li>
-            }
+            var phone = this.props.data.phone.map(function (eachPhone, ind) {
+                    return <li key={ind}>{eachPhone}</li>
+                }
+            );
+        }
+
+
+        return (
+            <section id="about">
+                <div className="row">
+                    <div className="three columns">
+                        <img className="profile-pic" src={profilepic} alt="Yan-Ru Profile Pic"/>
+                    </div>
+                    <div className="nine columns main-col">
+                        <h2>About Me</h2>
+
+                        <p>{bio}</p>
+                        <div className="row">
+                            <div className="columns contact-details">
+                                <h2>Contact Details</h2>
+                                <p className="address">
+                                    <span>{name}</span><br/>
+                                    <span>{phone}</span>
+                                    <span>{email}</span>
+                                </p>
+                            </div>
+                            <div className="columns download">
+                                <p>
+                                    <a href={PDF} target="_blank" className="button"><i className="fa fa-download"></i>Download
+                                        Resume</a>
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+            </section>
         );
     }
-
-
-
-
-    return (
-      <section id="about">
-      <div className="row">
-         <div className="three columns">
-            <img className="profile-pic"  src={profilepic} alt="Yan-Ru Profile Pic" />
-         </div>
-         <div className="nine columns main-col">
-            <h2>About Me</h2>
-
-            <p>{bio}</p>
-            <div className="row">
-               <div className="columns contact-details">
-                  <h2>Contact Details</h2>
-                  <p className="address">
-                      <span>{name}</span><br/>
-                      <span>{phone}</span>
-                      <span>{email}</span>
-                  </p>
-               </div>
-               <div className="columns download">
-                  <p>
-                     <a href={PDF} target = "_blank" className="button"><i className="fa fa-download"></i>Download Resume</a>
-                  </p>
-               </div>
-            </div>
-         </div>
-      </div>
-
-   </section>
-    );
-  }
 }
 
 export default About;
