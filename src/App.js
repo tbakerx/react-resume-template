129d3e6fad7dd037df60133a810d5596209ec546
jsking216 <jking@onecause.com>
2020-04-27 20:07:35 -0400

    add testMode flag

--- src/App.js
+++ src/App.js
@@ -19,7 +19,7 @@ class App extends Component {
       resumeData: {}
     };
 
-    ReactGA.initialize('UA-110570651-1');
+    ReactGA.initialize('UA-110570651-1', { testMode: process.env.NODE_ENV === 'test' });
     ReactGA.pageview(window.location.pathname);
 
   }
