32a54f80d4fc8b89e0007e4c9e9f9a43c714c348
Humza23 <79542316+Humza23@users.noreply.github.com>
2021-08-17 15:01:09 -0400

    wip

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -18,11 +18,6 @@ class Footer extends Component {
               {networks}
            </ul>
 
-           <ul className="copyright">
-              <li>&copy; Copyright 2017 Tim Baker</li>
-              <li>Design by <a title="Styleshout" href="http://www.styleshout.com/">Styleshout</a></li>
-           </ul>
-
         </div>
         <div id="go-top"><a className="smoothscroll" title="Back to Top" href="#home"><i className="icon-up-open"></i></a></div>
      </div>
--- src/Components/Header.js
+++ src/Components/Header.js
@@ -7,7 +7,7 @@ class Header extends Component {
       var name = this.props.data.name;
       var occupation= this.props.data.occupation;
       var description= this.props.data.description;
-      var city= this.props.data.address.city;
+      var state= this.props.data.address.state;
       var networks= this.props.data.social.map(function(network){
         return <li key={network.name}><a href={network.url}><i className={network.className}></i></a></li>
       })
@@ -35,7 +35,7 @@ class Header extends Component {
       <div className="row banner">
          <div className="banner-text">
             <h1 className="responsive-headline">I'm {name}.</h1>
-            <h3>I'm a {city} based <span>{occupation}</span>. {description}.</h3>
+            <h3>I'm a {state} based <span>{occupation}</span>. {description}.</h3>
             <hr />
             <ul className="social">
                {networks}
