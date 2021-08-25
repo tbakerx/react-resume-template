e81eee819242647955d31ebfde17f6f1e2d919c2
Humza23 <79542316+Humza23@users.noreply.github.com>
2021-08-25 14:29:44 -0400

    name

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -64,7 +64,7 @@ class Header extends Component {
 
         <div className="row banner">
           <div className="banner-text">
-            <h1 className="responsive-headline">I'm {name}.</h1>
+            <h1 className="responsive-headline">{name}</h1>
             <h3>
               I'm a {state} based <span>{occupation}</span>. {description}.
             </h3>
