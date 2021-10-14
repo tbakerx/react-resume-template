5be94fd81eb1dd30a6b4830e13e7c7a6e72acb12
ashutoshkailkhura <ashi3377@gmail.com>
2021-10-14 13:58:45 +0530

    change class into function

--- src/Components/About.js
+++ src/Components/About.js
@@ -1,55 +1,49 @@
-import React, { Component } from 'react';
+import React from "react";
 
-class About extends Component {
-  render() {
+export default function About({ data }) {
+  var name = data.name;
+  var profilepic = "https://github.com/ashutoshkailkhura.png";
+  var bio = data.bio;
+  var city = data.address.city;
+  var state = data.address.state;
+  var email = data.email;
 
-    if(this.props.data){
-      var name = this.props.data.name;
-      var profilepic= "images/"+this.props.data.image;
-      var bio = this.props.data.bio;
-      var street = this.props.data.address.street;
-      var city = this.props.data.address.city;
-      var state = this.props.data.address.state;
-      var zip = this.props.data.address.zip;
-      var phone= this.props.data.phone;
-      var email = this.props.data.email;
-      var resumeDownload = this.props.data.resumedownload;
-    }
-
-    return (
-      <section id="about">
+  return (
+    <section id="about">
       <div className="row">
-         <div className="three columns">
-            <img className="profile-pic"  src={profilepic} alt="Tim Baker Profile Pic" />
-         </div>
-         <div className="nine columns main-col">
-            <h2>About Me</h2>
-
-            <p>{bio}</p>
-            <div className="row">
-               <div className="columns contact-details">
-                  <h2>Contact Details</h2>
-                  <p className="address">
-						   <span>{name}</span><br />
-						   <span>{street}<br />
-						         {city} {state}, {zip}
-                   </span><br />
-						   <span>{phone}</span><br />
-                     <span>{email}</span>
-					   </p>
-               </div>
-               <div className="columns download">
-                  <p>
-                     <a href={resumeDownload} className="button"><i className="fa fa-download"></i>Download Resume</a>
-                  </p>
-               </div>
+        <div className="three columns">
+          <img className="profile-pic" src={profilepic} alt="ashutosh" />
+        </div>
+        <div className="nine columns main-col">
+          <h2>About Me</h2>
+          <p>{bio}</p>
+          <div className="row">
+            <div className="columns contact-details">
+              <h2>Contact Details</h2>
+              <p className="address">
+                <span>{name}</span>
+                <br />
+                <span>
+                  {city} {state}
+                </span>
+                <br />
+                <span>
+                  <a href="mailto:user@domain.com?Subject=Hello%20User">
+                    {email}
+                  </a>
+                </span>
+              </p>
             </div>
-         </div>
+            <div className="columns download">
+              {/* <p>
+                <a href={resumeDownload} className="button">
+                  <i className="fa fa-download"></i>Download Resume
+                </a>
+              </p> */}
+            </div>
+          </div>
+        </div>
       </div>
-
-   </section>
-    );
-  }
+    </section>
+  );
 }
-
-export default About;
