adfee390f15f2c1b16363d4a63f5e39a7ef5ec49
Brodie Brock <brodiebrock@gmail.com>
2021-12-14 16:40:56 -0600

    Updating resume data

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -8,6 +8,7 @@ class Header extends Component {
       var occupation= this.props.data.occupation;
       var description= this.props.data.description;
       var city= this.props.data.address.city;
+      var city2= this.props.data.address.city2;
       var networks= this.props.data.social.map(function(network){
         return <li key={network.name}><a href={network.url}><i className={network.className}></i></a></li>
       })
@@ -35,7 +36,7 @@ class Header extends Component {
       <div className="row banner">
          <div className="banner-text">
             <h1 className="responsive-headline">I'm {name}.</h1>
-            <h3>I'm a {city} based <span>{occupation}</span>. {description}.</h3>
+            <h3>I'm a {city2} based <span>{occupation}</span>. {description}.</h3>
             <hr />
             <ul className="social">
                {networks}
