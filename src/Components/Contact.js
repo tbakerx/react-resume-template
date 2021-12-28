6f3a80a9f40d5e0dd3b624d11e13c61f8566fa6f
scottmc500 <smchenry2014@gmail.com>
2021-12-28 15:39:15 -0500

    Update Contact.js

--- src/Components/Contact.js
+++ src/Components/Contact.js
@@ -9,7 +9,7 @@ class Contact extends Component {
       var city = this.props.data.address.city;
       var state = this.props.data.address.state;
       var zip = this.props.data.address.zip;
-      var phone= this.props.data.phone;
+      var phone = this.props.data.phone;
       var email = this.props.data.email;
       var message = this.props.data.contactmessage;
     }
