38250cf008c7bbaa291179fe18081f940201eb83
scottmc500 <smchenry2014@gmail.com>
2021-12-29 10:44:46 -0500

    Update Contact.js

--- src/Components/Contact.js
+++ src/Components/Contact.js
@@ -3,14 +3,14 @@ import React, { Component } from 'react';
 class Contact extends Component {
    render() {
 
-      if(this.props.data){
-       var name = this.props.data.name;
-       var street = this.props.data.address.street;
-       var city = this.props.data.address.city;
-       var state = this.props.data.address.state;
-       var zip = this.props.data.address.zip;
-       var phone = this.props.data.phone;
-       //var email = this.props.data.email;
+      if (this.props.data) {
+         var name = this.props.data.name;
+         var street = this.props.data.address.street;
+         var city = this.props.data.address.city;
+         var state = this.props.data.address.state;
+         var zip = this.props.data.address.zip;
+         var phone = this.props.data.phone;
+         //var email = this.props.data.email;
          var message = this.props.data.contactmessage;
       }
 
