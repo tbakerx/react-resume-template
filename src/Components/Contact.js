8d4f1aa7a644805ceb1d4daa1c922dca6a2061bd
scottmc500 <smchenry2014@gmail.com>
2021-12-28 22:33:26 -0500

    Editing stuff

--- src/Components/Contact.js
+++ src/Components/Contact.js
@@ -1,54 +1,42 @@
 import React, { Component } from 'react';
 
 class Contact extends Component {
-  render() {
+   render() {
 
-    if(this.props.data){
-      var name = this.props.data.name;
-      var street = this.props.data.address.street;
-      var city = this.props.data.address.city;
-      var state = this.props.data.address.state;
-      var zip = this.props.data.address.zip;
-      var phone = this.props.data.phone;
-      //var email = this.props.data.email;
-      var message = this.props.data.contactmessage;
-    }
+      if(this.props.data){
+       var name = this.props.data.name;
+       var street = this.props.data.address.street;
+       var city = this.props.data.address.city;
+       var state = this.props.data.address.state;
+       var zip = this.props.data.address.zip;
+       var phone = this.props.data.phone;
+       //var email = this.props.data.email;
+         var message = this.props.data.contactmessage;
+      }
 
-    return (
-      <section id="contact">
-
-         <div className="row section-head">
-
+      return (
+         <section id="contact">
+            <div className="row section-head">
             <div className="two columns header-col">
-
                <h1><span>Get In Touch.</span></h1>
-
             </div>
-
             <div className="ten columns">
-
                   <p className="lead">{message}</p>
-
             </div>
-
          </div>
 
          <div className="row">
             <div className="eight columns">
-
                <form action="" method="post" id="contactForm" name="contactForm">
 					<fieldset>
-
                   <div>
 						   <label htmlFor="contactName">Name <span className="required">*</span></label>
 						   <input type="text" defaultValue="" size="35" id="contactName" name="contactName" onChange={this.handleChange}/>
                   </div>
-
                   <div>
 						   <label htmlFor="contactEmail">Email <span className="required">*</span></label>
 						   <input type="text" defaultValue="" size="35" id="contactEmail" name="contactEmail" onChange={this.handleChange}/>
                   </div>
-
                   <div>
 						   <label htmlFor="contactSubject">Subject</label>
 						   <input type="text" defaultValue="" size="35" id="contactSubject" name="contactSubject" onChange={this.handleChange}/>
@@ -74,10 +62,8 @@ class Contact extends Component {
 				   </div>
            </div>
 
-
             <aside className="four columns footer-widgets">
                <div className="widget widget_contact">
-
 					   <h4>Address and Phone</h4>
 					   <p className="address">
 						   {name}<br />
@@ -87,9 +73,9 @@ class Contact extends Component {
 					   </p>
 				   </div>
             </aside>
-      </div>
-   </section>
-    );
+         </div>
+      </section>
+   );
   }
 }
 
