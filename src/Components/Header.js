29e8be52af26f409aa5282f54c6c946945a3b89f
Vadim <Vadim.Zaripov@senacor.com>
2022-04-24 04:25:48 +0200

    Changed im to i am

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -31,7 +31,7 @@ class Header extends Component {
 
       <div className="row banner">
          <div className="banner-text">
-            <h1 className="responsive-headline">Hello, I'm {name}.</h1>
+            <h1 className="responsive-headline">Hello, I am {name}.</h1>
             <h3>I'm a {city} based <span>{occupation}</span>. {description}.</h3>
             <hr />
             <ul className="social">
