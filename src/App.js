c6ddf9495df74a62b14f1ddd97f38da1aa1f4ef0
Vadim <Vadim.Zaripov@senacor.com>
2022-04-24 03:36:09 +0200

    - add deployment files - add cpnel config yml to automate deployment

--- src/App.js
+++ src/App.js
@@ -6,7 +6,6 @@ import Header from './Components/Header';
 import Footer from './Components/Footer';
 import About from './Components/About';
 import Resume from './Components/Resume';
-import Contact from './Components/Contact';
 
 class App extends Component {
 
