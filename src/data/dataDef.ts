a7383d094accf7351a7a5e19af9aff88bd1cff03
Tim Baker <tbakerx@gmail.com>
2022-07-21 07:49:11 -0700

    Add preliminary data model definition

---
+++ src/data/dataDef.ts
@@ -0,0 +1,125 @@
+import {FC, SVGProps} from 'react';
+
+import {IconProps} from '../components/Icon/Icon';
+
+export interface HomepageMeta {
+  title: string;
+  description: string;
+  ogImageUrl: string;
+  twitterCardType?: 'summary' | 'summary_large';
+  twitterTitle?: string;
+  twitterSite?: string;
+  twitterCreator?: string;
+  twitterDomain?: string;
+  twitterUrl?: string;
+  twitterDescription?: string;
+  twitterImageUrl?: string;
+}
+
+/**
+ * Hero section
+ */
+export interface Hero {
+  imageSrc: string;
+  name: string;
+  description: JSX.Element;
+  actions: HeroActionItem[];
+}
+
+interface HeroActionItem {
+  href: string;
+  text: string;
+  primary?: boolean;
+  Icon?: (props: SVGProps<SVGSVGElement>) => JSX.Element;
+}
+
+/**
+ * Stat section
+ */
+export interface Stat {
+  title: string;
+  value: number;
+  Icon?: (props: SVGProps<SVGSVGElement>) => JSX.Element;
+}
+
+/**
+ * Skills section
+ */
+
+export interface Skill {
+  name: string;
+  level: number;
+  max?: number;
+}
+
+export interface SkillGroup {
+  name: string;
+  skills: Skill[];
+}
+
+/**
+ * Portfolio section
+ */
+export interface PortfolioItem {
+  title: string;
+  category: {
+    name: PortfolioCategory;
+    url: string;
+  };
+  description: string;
+  url: string;
+  imageUrl: string;
+}
+
+type PortfolioCategory = 'Tool' | 'App' | 'Service';
+
+/**
+ * Resume section
+ */
+export interface TimelineItem {
+  date: string;
+  location: string;
+  title: string;
+  content: JSX.Element;
+}
+
+/**
+ * Contact section
+ */
+export interface ContactSection {
+  headerText?: string;
+  description: string;
+}
+
+export const ContactType = {
+  Email: 'Email',
+  Phone: 'Phone',
+  Location: 'Location',
+  Github: 'Github',
+  LinkedIn: 'LinkedIn',
+  Facebook: 'Facebook',
+  Twitter: 'Twitter',
+  Instagram: 'Instagram',
+} as const;
+
+export type ContactType = typeof ContactType[keyof typeof ContactType];
+
+export interface ContactItem {
+  type: ContactType;
+  text: string;
+  href?: string;
+}
+
+export interface ContactValue {
+  Icon: FC<IconProps> | ((props: SVGProps<SVGSVGElement>) => JSX.Element);
+  srLabel: string;
+}
+
+/**
+ * Social items
+ */
+export interface Social {
+  label: string;
+  Icon: FC<IconProps>;
+  href: string;
+}
