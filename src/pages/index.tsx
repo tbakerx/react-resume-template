0fbbcea38f605ac5e09fda47dcabdb0a99e2d900
Tim Baker <tbakerx@gmail.com>
2022-08-30 08:09:07 -0700

    Add footer section

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,6 +3,7 @@ import {FC, memo} from 'react';
 import Page from '../components/Layout/Page';
 import About from '../components/Sections/About';
 import Contact from '../components/Sections/Contact';
+import Footer from '../components/Sections/Footer';
 import Hero from '../components/Sections/Hero';
 import Portfolio from '../components/Sections/Portfolio';
 import Resume from '../components/Sections/Resume';
@@ -20,6 +21,7 @@ const Home: FC = memo(() => {
       <Portfolio />
       <Testimonials />
       <Contact />
+      <Footer />
     </Page>
   );
 });
