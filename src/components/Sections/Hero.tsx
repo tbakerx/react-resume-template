1b093a650bc573de508a04fbd0b5638091c575a1
Tim Baker <tbakerx@gmail.com>
2022-08-30 08:09:07 -0700

    Add footer section

--- src/components/Sections/Hero.tsx
+++ src/components/Sections/Hero.tsx
@@ -26,7 +26,7 @@ const Hero: FC = memo(() => {
           <div className="flex flex-col items-center gap-y-6 rounded-xl bg-gray-800/40 p-6 text-center shadow-lg backdrop-blur-sm">
             <h1 className="text-4xl font-bold text-white sm:text-5xl lg:text-7xl">{name}</h1>
             {description}
-            <div className="flex gap-x-4">
+            <div className="flex gap-x-4 text-neutral-100">
               <Socials />
             </div>
             <div className="flex w-full justify-center gap-x-4">
--- src/components/Socials.tsx
+++ src/components/Socials.tsx
@@ -8,7 +8,7 @@ const Socials: FC = memo(() => {
       {socialLinks.map(({label, Icon, href}) => (
         <a
           aria-label={label}
-          className="-m-1.5 rounded-md p-1.5 text-white transition-all duration-300 hover:text-orange-500 focus:outline-none focus:ring-2 focus:ring-orange-500  sm:-m-3 sm:p-3"
+          className="-m-1.5 rounded-md p-1.5 transition-all duration-300 hover:text-orange-500 focus:outline-none focus:ring-2 focus:ring-orange-500  sm:-m-3 sm:p-3"
           href={href}
           key={label}>
           <Icon className="h-5 w-5 align-baseline sm:h-6 sm:w-6" />
