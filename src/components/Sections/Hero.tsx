25b08479ddcc6e26790cd0c39a7a304ff7e1d03c
Tim Baker <tbakerx@gmail.com>
2022-09-08 08:22:50 -0700

    Fix parallax on iOS

--- src/components/Sections/Hero.tsx
+++ src/components/Sections/Hero.tsx
@@ -33,7 +33,7 @@ const Hero: FC = memo(() => {
               {actions.map(({href, text, primary, Icon}) => (
                 <a
                   className={classNames(
-                    'flex gap-x-2 rounded-full border-2 bg-none py-1 px-2 text-sm font-medium text-white ring-offset-gray-700/80 hover:bg-gray-700/80 focus:outline-none focus:ring-2 focus:ring-offset-2 sm:py-2 sm:px-4 sm:text-base',
+                    'flex gap-x-2 rounded-full border-2 bg-none py-2 px-4 text-sm font-medium text-white ring-offset-gray-700/80 hover:bg-gray-700/80 focus:outline-none focus:ring-2 focus:ring-offset-2 sm:text-base',
                     primary ? 'border-orange-500 ring-orange-500' : 'border-white ring-white',
                   )}
                   href={href}
