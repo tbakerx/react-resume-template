a6e0160f37d9860a4b8b4f8eb4ae5a7d164bb84a
Subham Mishra <63531369+msubham193@users.noreply.github.com>
2023-03-17 13:39:33 +0530

    Horizontal Scroll Fixed

--- src/data/data.tsx
+++ src/data/data.tsx
@@ -69,7 +69,7 @@ export type SectionId = typeof SectionId[keyof typeof SectionId];
  */
 export const heroData: Hero = {
   imageSrc: heroImage,
-  name: `I'm Tim Baker.`,
+  name: `I'm Subham Mishra.`,
   description: (
     <>
       <p className="prose-sm text-stone-200 sm:prose-base lg:prose-lg">
