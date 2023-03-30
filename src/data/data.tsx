924fed89afcea6719f530089b53dbb36b28cceed
BillStone <bill.stone1@live.com>
2023-03-30 14:42:25 -0500

    Initial Commit for Bill S

--- src/data/data.tsx
+++ src/data/data.tsx
@@ -1,10 +1,10 @@
 import {
   AcademicCapIcon,
-  CalendarIcon,
+  //CalendarIcon,
   DownloadIcon,
   FlagIcon,
-  MapIcon,
-  OfficeBuildingIcon,
+  //MapIcon,
+  //OfficeBuildingIcon,
   SparklesIcon,
 } from '@heroicons/react/outline';
 
@@ -69,18 +69,21 @@ export type SectionId = typeof SectionId[keyof typeof SectionId];
  */
 export const heroData: Hero = {
   imageSrc: heroImage,
-  name: `I'm Tim Baker.`,
+  name: `Hi, I'm Bill Stone.`,
   description: (
     <>
       <p className="prose-sm text-stone-200 sm:prose-base lg:prose-lg">
-        I'm a Victoria based <strong className="text-stone-100">Full Stack Software Engineer</strong>, currently working
-        at <strong className="text-stone-100">Instant Domains</strong> helping build a modern, mobile-first, domain
-        registrar and site builder.
+        I am a <strong className="text-stone-100">Full Stack Software Engineer</strong> based out of Texas.<br></br> I'm
+        currently
+        <strong className="text-stone-100"> open to new opportunities </strong> as a <br></br>{' '}
+        <strong className="text-stone-100">Full Stack Software Engineer </strong> or{' '}
+        <strong className="text-stone-100">Full Stack Software Lead </strong> <br></br>I am ready to aid in building
+        your modern new application, or refactoring your existing one.
       </p>
       <p className="prose-sm text-stone-200 sm:prose-base lg:prose-lg">
-        In my free time time, you can catch me training in <strong className="text-stone-100">Muay Thai</strong>,
-        plucking my <strong className="text-stone-100">banjo</strong>, or exploring beautiful{' '}
-        <strong className="text-stone-100">Vancouver Island</strong>.
+        I spend my free time keeping up to date on newer technlogies, walking my dogs, <br></br> visiting a{' '}
+        <strong className="text-stone-100">GREAT </strong>
+        new place in our RV with my wife <strong className="text-stone-100">Norma</strong>, or visiting our family.
       </p>
     </>
   ),
@@ -104,16 +107,17 @@ export const heroData: Hero = {
  */
 export const aboutData: About = {
   profileImageSrc: profilepic,
-  description: `Use this bio section as your way of describing yourself and saying what you do, what technologies you like
-  to use or feel most comfortable with, describing your personality, or whatever else you feel like throwing
-  in.`,
+  description: `Results-oriented leader with 25+ years of global experience architecting and developing database and application solutions in multiple industries while also managing remote teams. BS in Computer Information Systems with deep experience in designing, coding, testing, debugging, documenting, and supporting software. Adept at finding reliable solutions to software requirements and issues; creating quality, fully tested artifacts; identifying ways in which to improve processes; and presenting ideas to management/teams.
+
+  
+`,
   aboutItems: [
-    {label: 'Location', text: 'Victoria, BC', Icon: MapIcon},
-    {label: 'Age', text: '29', Icon: CalendarIcon},
-    {label: 'Nationality', text: 'Canadian / Irish', Icon: FlagIcon},
-    {label: 'Interests', text: 'Motorcycles, Muay Thai, Banjos', Icon: SparklesIcon},
-    {label: 'Study', text: 'University of Victoria', Icon: AcademicCapIcon},
-    {label: 'Employment', text: 'Instant Domains, inc.', Icon: OfficeBuildingIcon},
+    {label: 'Mentoring', text: 'Teams up to 23 developers', Icon: SparklesIcon},
+    {label: 'Coaching', text: 'On Shore and OffShore teams', Icon: FlagIcon},
+    {label: 'Nationality', text: 'American - US citizen', Icon: FlagIcon},
+    {label: 'Interests', text: 'Family, Travel, Software Engineering', Icon: SparklesIcon},
+    {label: 'Study', text: 'DeVry University: Irving, TX USA', Icon: AcademicCapIcon},
+    {label: 'Employment', text: 'Open To Opportunities', Icon: SparklesIcon},
   ],
 };
 
@@ -314,17 +318,20 @@ export const testimonial: TestimonialSection = {
   testimonials: [
     {
       name: 'John Doe',
-      text: 'Use this as an opportunity to promote what it is like to work with you. High value testimonials include ones from current or past co-workers, managers, or from happy clients.',
+      text:
+        'Use this as an opportunity to promote what it is like to work with you. High value testimonials include ones from current or past co-workers, managers, or from happy clients.',
       image: 'https://cloudflare-ipfs.com/ipfs/Qmd3W5DuhgHirLHGVixi6V76LhCkZUz6pnFt5AJBiyvHye/avatar/169.jpg',
     },
     {
       name: 'Jane Doe',
-      text: 'Here you should write some nice things that someone has said about you. Encourage them to be specific and include important details (notes about a project you were on together, impressive quality produced, etc).',
+      text:
+        'Here you should write some nice things that someone has said about you. Encourage them to be specific and include important details (notes about a project you were on together, impressive quality produced, etc).',
       image: 'https://cloudflare-ipfs.com/ipfs/Qmd3W5DuhgHirLHGVixi6V76LhCkZUz6pnFt5AJBiyvHye/avatar/14.jpg',
     },
     {
       name: 'Someone else',
-      text: 'Add several of these, and keep them as fresh as possible, but be sure to focus on quality testimonials with strong highlights of your skills/work ethic.',
+      text:
+        'Add several of these, and keep them as fresh as possible, but be sure to focus on quality testimonials with strong highlights of your skills/work ethic.',
       image: 'https://cloudflare-ipfs.com/ipfs/Qmd3W5DuhgHirLHGVixi6V76LhCkZUz6pnFt5AJBiyvHye/avatar/69.jpg',
     },
   ],
