90170b5da30490533383e135916921726deba953
Bibin Alias <bibin.alias.jz@bp.renesas.com>
2023-04-19 17:32:19 +0530

    resolve an error of jesse pic

--- src/data/data.tsx
+++ src/data/data.tsx
@@ -26,7 +26,6 @@ import porfolioImage9 from '../images/portfolio/portfolio-9.jpg';
 import porfolioImage10 from '../images/portfolio/portfolio-10.jpg';
 import porfolioImage11 from '../images/portfolio/portfolio-11.jpg';
 import profilepic from '../images/IMG_17002.jpg';
-import picjessie from '../images/IMG_17002.jpg';
 import testimonialImage from '../images/testimonial.webp';
 import {
   About,
