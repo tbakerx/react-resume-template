ecbcda4d23e1764e10be52fc7dbed61088a99f5e
Webster Avosa <websterb17@gmail.com>
2023-08-01 15:42:52 -0400

    Update src/components/Sections/Footer.tsx
    Co-authored-by: Tim Baker <tbakerx@gmail.com>
    Signed-off-by: Webster Avosa <websterb17@gmail.com>

--- src/components/Sections/Footer.tsx
+++ src/components/Sections/Footer.tsx
@@ -20,7 +20,7 @@ const Footer: FC = memo(() => {
         <div className="flex gap-x-4 text-neutral-500">
           <Socials />
         </div>
-        <span className="text-sm text-neutral-700">© {`Copyright ${currentYear}`} Tim Baker</span>
+        <span className="text-sm text-neutral-700">© Copyright {currentYear} Tim Baker</span>
       </div>
     </div>
   );
