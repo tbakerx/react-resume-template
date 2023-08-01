9311aa080cd500312c13d8eb19d4b5b9e461f712
Tim Baker <tbakerx@gmail.com>
2023-08-01 08:09:01 -0700

    Full yarn dep upgrade

--- next.config.js
+++ next.config.js
@@ -20,11 +20,7 @@ const nextConfig = {
   pageExtensions: ['tsx', 'mdx', 'ts'],
   poweredByHeader: false,
   productionBrowserSourceMaps: false,
-  svgo: {
-    multipass: true,
-    plugins: ['removeDimensions'],
-  },
-  strictMode: true,
+  reactStrictMode: true,
   swcMinify: true,
   trailingSlash: false,
   images: {
